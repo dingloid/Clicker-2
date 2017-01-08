b4ad0ed5067d0a9854352eefc05250c27699bc9a
Chad Smith <Smith.Chad12@gmail.com>
2017-01-08 01:03:31 -0600

    small change

--- app/src/main/java/ohpiestudio/clicker2/Adapter/SkinsAdapter.java
+++ app/src/main/java/ohpiestudio/clicker2/Adapter/SkinsAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+
 import ohpiestudio.clicker2.Upgrades.Skins;
 
 public class SkinsAdapter extends BaseAdapter {
